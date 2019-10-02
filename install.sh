b3ff0bd3b4ebb73ca6ce2cd5d7753d288254659d
Joash Christian Liwanag <nightlock0326@gmail.com>
2019-10-02 19:32:11 +0800

    Fix dotfiles remote link

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ fi
 # Clone the dotfiles git if it's not found
 if [ ! -f $HOME/.dotfiles ]; then
 	echo "Dotfiles directory not found. Cloning git repo."
-	git clone https://github.com/AJigsawnHalo/dotfiles.git .dotfiles
+	git clone https://github.com/AJigsawnHalo/dotfiles-akagi.git .dotfiles
 fi
 
 # Download plugins and themes for vim and zsh
